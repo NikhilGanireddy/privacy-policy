45b989d08476669d248d15ce09aa0625b9e9849d
nikhilganireddy <nikhilganireddy@yahoo.com>
2024-05-31 18:25:49 +0530

    commit-1

--- app/page.js
+++ app/page.js
@@ -1,112 +1,58 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className="flex min-h-screen flex-col items-center p-12 md:p-24 gap-12">
+      <div className="flex flex-col items-center gap-4">
+        <h1 className="text-4xl font-bold">Session Share - thegdp</h1>
+        <h2 className="text-sm">Effective Date: May 31, 2024</h2>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <p className="text-left  w-full">
+      Thank you for choosing to use Session Share - thegdp. Your privacy is important to us. This Privacy Policy explains how we collect, use, and protect your information when you use our Chrome extension.
+      </p>
+      <div className="gap-4 w-full flex justify-center items-center flex-col text-left">
+        <h1 className="w-full font-bold">Information We Collect:</h1>
+        <p className="text-left w-full">
+        <b>Cookies and Session Data:</b> The only data we collect from our users is the cookie information and session data necessary to enable the sharing of browser sessions across multiple devices. We do not collect any personal information beyond what is necessary for the functionality of the extension.
+      </p>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="gap-4 w-full flex justify-center items-center flex-col text-left">
+        <h1 className="w-full font-bold">Use of Information:</h1>
+        <p className="text-left w-full">
+        The information we collect is solely used to facilitate the sharing of your browser sessions across devices. It enables seamless access to your sessions, improving your workflow and interaction with platforms such as Chegg and TutorBin.
+      </p>
+      </div>
+      <div className="gap-4 w-full flex justify-center items-center flex-col text-left">
+        <h1 className="w-full font-bold">Protection of Information:</h1>
+        <p className="text-left w-full">
+        We implement a variety of security measures to maintain the safety of your session information. However, no method of transmission over the Internet or method of electronic storage is 100% secure. While we strive to use commercially acceptable means to protect your information, we cannot guarantee its absolute security.
+      </p>
+      </div>
+      <div className="gap-4 w-full flex justify-center items-center flex-col text-left">
+        <h1 className="w-full font-bold">Sharing of Information:</h1>
+        <p className="text-left w-full">
+        We do not sell, trade, or otherwise transfer to outside parties your session information. This does not include trusted third parties who assist us in operating our extension, conducting our business, or serving our users, so long as those parties agree to keep this information confidential.
+      </p>
+      </div>
+      <div className="gap-4 w-full flex justify-center items-center flex-col text-left">
+        <h1 className="w-full font-bold">Your Consent:</h1>
+        <p className="text-left w-full">
+        By using our extension, you consent to our privacy policy.
+      </p>
+      </div>
+      <div className="gap-4 w-full flex justify-center items-center flex-col text-left">
+        <h1 className="w-full font-bold">Changes to our Privacy Policy:</h1>
+        <p className="text-left w-full">
+        Any changes we may make to our privacy policy in the future will be posted on this page. If we make material changes to how we treat our users' personal information, we will notify you through a notice on the extension and/or by email.
+      </p>
+      </div>
+      <div className="gap-4 w-full flex justify-center items-center flex-col text-left">
+        <h1 className="w-full font-bold">Contact Us:</h1>
+        <p className="text-left w-full">
+        If you have any questions regarding this privacy policy, you may contact us using the information below:
+      </p>
+      <Link className="text-red-500 w-full text-left" href={"https://nikhilganireddy.vercel.app/contact"}>Contact Page</Link>
       </div>
     </main>
   );
